eed1c416c23d23c91c9426e2b18828753a023d22
johndavidhays-ctrl <johndavidhays@gmail.com>
2025-07-30 13:42:32 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BusinessLocator from "./BusinessLocator";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BusinessLocator />
+  </React.StrictMode>
+);
